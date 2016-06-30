3181c82a6662a2e222d6ca969d29db63273207a3
cherishhsieh <xieqin1991@gmail.com>
2016-06-30 13:33:15 +0800

    fix bugs

--- cherish-slider.js
+++ cherish-slider.js
@@ -1,9 +1,11 @@
 (function(exports, $) {
   'use strict'
 
-  var _el, _config = {
-    count: 0,
-    time: 1000
+  var _el,
+  intervalId,
+  _config = {
+    index: 0, //轮播初始设置
+    time: 1000 //轮播间隔
   }
 
   var Slider = function (el, opts) {
@@ -20,26 +22,69 @@
   }
 
   function _change () {
-    var height = _config.count * 100
+    var height = _config.index * 100
     $(_el).css('top', '-' + height + 'px')
     $("#slider-index li").removeClass('active')
-    $("#slider-index li").eq(_config.count).addClass('active')
-    _config.count++
+    $("#slider-index li").eq(_config.index).addClass('active')
+  }
+
+  function _stop () {
+    clearInterval(intervalId)
   }
 
   function _start () {
-    setInterval(function() {
-      _config.count = ((_config.count + 1) >= 3) ? (_config.count + 1 - 3) : (_config.count + 1)
+    renderIndex()
+
+    $('#slider-index li').on('mouseover', function() {
+      _stop ()
+    })
+
+    $('#slider-index li').on('mouseout', function() {
+      _animation ()
+    })
+
+    $('#slider-index li').on('click', function() {
+      var i = $(this).index() - 0
+      _stop ()
+      _config.index = i
+      _change ()
+    })
+
+    _animation ()
+  }
+
+  function _animation () {
+    intervalId = setInterval(function() {
+      _config.index = ((_config.index + 1) >= _config.length) ? (_config.index + 1 - _config.length) : (_config.index + 1)
       _change()
     }, _config.time)
+  }
+
+  function renderIndex() {
+    var html = [
+      '<ol id="slider-index">',
+      '</ol>'
+    ].join('')
+    $(_el).parent().append(html)
+    var ol_html = ''
+    for(var i = 0; i < _config.length; i ++) {
+      ol_html += '<li> ' + (i + 1) + '</li>'
+    }
+    $('#slider-index').append(ol_html)
+    _change ()
   }
 
   Slider.prototype = {
     init: function (el, opts) {
       _el = el
       _config = _extend (opts, _config)
+      _config.length = $(_el).children().length - 0
       return this
     },
+    /**
+     * [start 启动轮播]
+     * @return {[type]} [description]
+     */
     start: function () {
       _start()
     }
