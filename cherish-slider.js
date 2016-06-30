21f9db7a70b9796e7291b188e0b75e4ef23d3a16
cherishhsieh <xieqin1991@gmail.com>
2016-06-30 12:02:16 +0800

    add slider

--- cherish-slider.js
+++ cherish-slider.js
@@ -1,22 +1,47 @@
 (function(exports, $) {
   'use strict'
 
-  var _config = {}
-  var _el
+  var _el, _config = {
+    count: 0,
+    time: 1000
+  }
 
   var Slider = function (el, opts) {
     return new Slider.prototype.init(el, opts)
   }
 
+  function _extend (o, n) {
+    for (var p in n) {
+      if (n.hasOwnProperty(p) && !o.hasOwnProperty(p)) {
+        o[p]=n[p]
+      }
+    }
+    return o
+  }
+
+  function _change () {
+    var height = _config.count * 100
+    $(_el).css('top', '-' + height + 'px')
+    $("#slider-index li").removeClass('active')
+    $("#slider-index li").eq(_config.count).addClass('active')
+    _config.count++
+  }
+
+  function _start () {
+    setInterval(function() {
+      _config.count = ((_config.count + 1) >= 3) ? (_config.count + 1 - 3) : (_config.count + 1)
+      _change()
+    }, _config.time)
+  }
+
   Slider.prototype = {
-    init: function(el, opts) {
+    init: function (el, opts) {
       _el = el
-      _config = opts
+      _config = _extend (opts, _config)
       return this
     },
-    start: function() {
-      console.log(_el)
-      console.log(_config)
+    start: function () {
+      _start()
     }
   }
 
